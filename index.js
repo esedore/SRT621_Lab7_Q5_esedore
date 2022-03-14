6e7d2e9cef0086b9ffc2eefaf5a3e7dab33af2c9
DPI910Group5 <dpi910_group5@protonmail.com>
2022-03-14 16:17:33 -0400

    Timestamp

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const http = require('http');
 const currentDate = new Date();
-const timestamp = currentDate.getTime();
+const strDate = currentDate.toString();
 const hostname = '127.0.0.1';
 const port = 8989;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('<script> var currentDate = Date(); stringDate = currentDate.toString(); document.write("Current Timestamp"+ stringDate);</script>');
+  res.write(strDate);
+  res.end('');
 });
-
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-  console.log(currentDate());
-  console.log(timestamp());
 });
